baf9c613b088e534f1f680188f19f07361c08e6c
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2024-01-02 21:09:17 -0500

    Generate HTML with JS

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-const products = [
+export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -15,7 +15,7 @@ const products = [
     ]
   },
   {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
     image: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
